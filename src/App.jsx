87abac44bf5b16909f22647608032633cb1ec134
Andreasberg89 <andber9@gmail.com>
2024-03-10 18:08:18 +0100

    Baisc layout setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,34 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Layout from './components/Layout'
 import Home from './components/Home'
+import { useEffect, useState } from 'react'
 
 
 function App() {
+
+  const [content, setContent] = useState([])
+  const [query, setQuery] = useState("James+Bond")
+
+  const getData = async() =>{
+    try{
+      const response = await fetch(`https://openlibrary.org/search.json?title=${query}&limit=20`)
+      const data = await response.json()
+      setContent(data.docs)
+    } catch{
+      console.error("Error i getData funksjon")
+    }
+  }
+
+
+  useEffect(() => {
+    getData()
+  }, [])
 
   return (
     <Layout>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home/>}/>
+          <Route index element={<Home content={content}/>}/>
         </Routes>
       </BrowserRouter>
     </Layout>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,20 @@
-export default function Home(){
+export default function Home({content}){
+
+    console.log(content)
+
     return(
         <>
-        <p>Home!</p>
+            <section>
+                {content?.map((book, index) =>
+                <article key={"book"+ index}>
+                    <h3>{book.title}</h3>
+                    <h4>Author:</h4>
+                    {book?.author_name?.map((author, idx) =>
+                    <li key={"author" + idx}>{author}</li>)}
+                    <p>First published: {book.first_publish_year}</p>
+                </article>)}
+            </section>
+            <p>Home!</p>
         </>
     )
 }
